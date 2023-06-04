cd7d07a529cfa8d2013c03abb1fb85e80412bb1b
Mahak491 <shreemahak01@gmail.com>
2023-06-04 14:56:57 +0530

    built header, card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const Header = () =>{
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+        <img className='logo' 
+        src='https://t3.ftcdn.net/jpg/02/41/30/72/240_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg'></img>
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            About Us
+          </li>
+          <li>
+            Contact Us
+          </li>
+          <li>
+            Cart
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const ReastrauntCard = () =>{
+  return (
+    <div className='res-card'>
+      <img className='res-logo'
+      alt="food-image" src='https://tse2.mm.bing.net/th?id=OIP.maQpFJiRuDMauaTZ3N7KiQHaEo&pid=Api&P=0&h=180'></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, cuisine, North Indian</h4>
+      <h4>4.2</h4>
+    </div>
+  )
+}
+
+const Body = () =>{
+  return(
+  <div className='body'>
+    <div className='search'>Search</div>
+    <div className='res-conatiner'>
+      <ReastrauntCard/>
+    </div>
+  </div>    
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Body/>
     </div>
   );
 }
